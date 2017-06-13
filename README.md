d6078d1e23952ff7cc0f492cc43871c1f1e09b61
Jordyn Marcellus <jordynmarcellus@gmail.com>
2017-06-12 21:28:05 -0400

    First commit, ejected create-react-app and am going fully nuts

---
+++ README.md
@@ -0,0 +1,7 @@
+#A digital lean canvas
+
+Why? Because I found it on TWG's blog and thought it was super cool! 
+
+Reading more about the methods, I decided to try and make this digital version. Good way to sloooowly read this 40-page PDF https://leanstack.com/LeanCanvas.pdf and do some work on it. 
+
+Uses React and Redux, plus CSS Grid for the layout. 
---
+++ src/state/lean-canvas.js
@@ -0,0 +1,17 @@
+import { State } from 'jumpstate'
+
+export default State({
+
+    initial: {
+        lean_canvas_problems : [],
+        lean_canvas_customer_segments : [],
+        lean_canvas_unique_value : "",
+        lean_canvas_solutions : [],
+        lean_canvas_channels : [],
+        lean_canvas_cost_structure : [],
+        lean_canvas_revenue_streams : [],
+        lean_canvas_key_metrics : [],
+        lean_canvas_unfair_advantage : [],
+    }
+
+})
